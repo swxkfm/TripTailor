6a07dbaeb8db01544780ffb02e2d72b2e58ce33c
swxkfm <116090187+swxkfm@users.noreply.github.com>
2025-10-22 10:30:05 +0800

    Update combination.sh

--- TripTailor/planner/combination.sh
+++ TripTailor/planner/combination.sh
@@ -1,9 +1,17 @@
-export OUTPUT_DIR=""
-export MODEL_NAME=""
-# SET_TYPE in ['validation', 'test']
-export SET_TYPE=test
-# STRATEGY in ['direct','cot','react','reflexion']
-export STRATEGY=reflexion
-export SUBMISSION_FILE=""
+OUTPUT_DIR="./outputs"
+MODEL_NAME=""
+SET_TYPE="test"
+STRATEGY="react"
+SUBMISSION_FILE="./outputs/${MODEL_NAME}_${STRATEGY}_result.json"
 
-python combination.py  --set_type $SET_TYPE --output_dir $OUTPUT_DIR --model_name $MODEL_NAME --strategy $STRATEGY --submission_file $SUBMISSION_FILE
+QUERY_DATA_PATH="../agents/outputs/direct_input.json"
+SELECTED_DATA_PATH="../database_EN/test.json"
+
+python combination.py \
+    --set_type "$SET_TYPE" \
+    --output_dir "$OUTPUT_DIR" \
+    --model_name "$MODEL_NAME" \
+    --strategy "$STRATEGY" \
+    --submission_file "$SUBMISSION_FILE" \
+    --query_data_path "$QUERY_DATA_PATH" \
+    --selected_data_path "$SELECTED_DATA_PATH"
