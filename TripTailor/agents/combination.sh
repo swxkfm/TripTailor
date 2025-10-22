862e35ded9da08ad0a1690ded97060ec075af752
swxkfm <116090187+swxkfm@users.noreply.github.com>
2025-10-22 10:36:38 +0800

    Update combination.sh

--- TripTailor/agents/combination.sh
+++ TripTailor/agents/combination.sh
@@ -1,10 +1,8 @@
-export INPUT_FILE="input_file"
-export OUTPUT_DIR="output_dir"
-export MODEL_NAME="model_name"
-# SET_TYPE in ['validation', 'test']
+export INPUT_FILE="../database_EN/test.json"
+export OUTPUT_DIR="../agents/outputs"
+export MODEL_NAME="qwen-plus"
 export SET_TYPE=test
-# STRATEGY in ['direct','cot','react','reflexion']
 export STRATEGY=direct
-export SUBMISSION_FILE="submission_file"
+export SUBMISSION_FILE="./outputs/qwen-plus_direct_result.json"
 
 python combination.py  --set_type $SET_TYPE --input_file $INPUT_FILE --output_dir $OUTPUT_DIR --model_name $MODEL_NAME --strategy $STRATEGY --submission_file $SUBMISSION_FILE
